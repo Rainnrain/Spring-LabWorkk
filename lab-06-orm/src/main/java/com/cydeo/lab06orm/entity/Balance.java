8cd7d6ba7bcb41c439f21971fb8b061188698cc1
Lorraine <lorrainecannom@gmail.com>
2022-10-28 11:52:45 -0400

    ORM practice

--- lab-06-orm/src/main/java/com/cydeo/lab06orm/entity/Balance.java
+++ lab-06-orm/src/main/java/com/cydeo/lab06orm/entity/Balance.java
@@ -4,6 +4,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.Entity;
+import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 
 @Entity
--- lab-06-orm/src/main/java/com/cydeo/lab06orm/entity/Customer.java
+++ lab-06-orm/src/main/java/com/cydeo/lab06orm/entity/Customer.java
@@ -3,9 +3,7 @@ package com.cydeo.lab06orm.entity;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.OneToMany;
-import javax.persistence.OneToOne;
+import javax.persistence.*;
 import java.util.List;
 
 @Entity
@@ -16,8 +14,6 @@ public class Customer extends BaseEntity {
     private String firstName;
     private String lastName;
     private String userName;
-    @OneToOne
-    private Balance balance;
     @OneToMany(mappedBy = "customer")
     private List<Address> address;
 }
