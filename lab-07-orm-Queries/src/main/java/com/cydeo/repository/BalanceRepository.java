6e07c5c5e18ae053d1e698f6e05018731457862e
Lorraine <lorrainecannom@gmail.com>
2022-11-01 21:05:30 -0400

    Query practice

---
+++ lab-07-orm-Queries/src/main/java/com/cydeo/repository/BalanceRepository.java
@@ -0,0 +1,15 @@
+package com.cydeo.repository;
+
+
+import org.springframework.stereotype.Repository;
+import javax.persistence.Entity;
+
+
+@Repository
+public interface BalanceRepository {
+    //Write a derived query to check balance exists for specific customer
+    //Write a derived query to get balance for specific customer
+    //Write a native query to get top 5 max balance
+    //Write a derived query to get all balances greater than or equal specific balance amount
+    //Write a native query to get all balances less than specific balance amount
+}
